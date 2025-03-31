ee39650a399b70ff2c7a0dcc5c7dbe1a283be208
mahendrak070 <krishna.koneru2003@gmail.com>
2025-03-31 18:25:22 -0400

    Better UI

--- lib/aquarium.dart
+++ lib/aquarium.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 import 'fish.dart';
-import 'database_helper.dart'; // Import the database helper
+import 'database_helper.dart';
 
 class AquariumScreen extends StatefulWidget {
   @override
@@ -54,8 +54,6 @@ class _AquariumScreenState extends State<AquariumScreen>
         selectedColor = Colors.red;
       }
       int fishCount = settings['fishCount'];
-
-      // Add fish based on the saved count
       for (int i = 0; i < fishCount; i++) {
         fishList.add(Fish(color: selectedColor, speed: selectedSpeed));
       }
@@ -82,10 +80,10 @@ class _AquariumScreenState extends State<AquariumScreen>
   void _updateFishPositions() {
     setState(() {
       for (var fish in fishList) {
-        fish.moveFish(); // Move fish
+        fish.moveFish();
       }
       if (collisionEffectEnabled) {
-        _checkAllCollisions(); // Check for collisions if enabled
+        _checkAllCollisions();
       }
     });
   }
@@ -97,9 +95,7 @@ class _AquariumScreenState extends State<AquariumScreen>
       fish1.changeDirection();
       fish2.changeDirection();
       setState(() {
-        fish1.color = Random().nextBool()
-            ? Colors.red
-            : Colors.green; // Random color change
+        fish1.color = Random().nextBool() ? Colors.red : Colors.green;
       });
     }
   }
@@ -123,145 +119,163 @@ class _AquariumScreenState extends State<AquariumScreen>
     // Get screen size for responsiveness
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
-
+    
     return Scaffold(
+      backgroundColor: Colors.white, // Overall white background
       appBar: AppBar(
-        title: Center(
-          child: Text('Virtual Aquarium'),
-        ),
+        backgroundColor: Colors.white,
+        elevation: 1,
+        title: Text('Virtual Aquarium',
+            style: TextStyle(color: Colors.black)),
+        centerTitle: true,
+        iconTheme: IconThemeData(color: Colors.black),
       ),
-      body: Stack(
-        children: [
-          Column(
-            children: [
-              SizedBox(
-                  height: screenHeight * 0.02), // Responsive space at the top
-
-              // Container that represents the aquarium
-              Center(
-                child: Container(
-                  width: screenWidth * 0.8, // 80% of the screen width
-                  height: screenHeight * 0.4, // 40% of the screen height
-                  decoration: BoxDecoration(
-                    color: Colors.blue, // Set the aquarium background to blue
-                    border: Border.all(color: Colors.white),
-                    borderRadius:
-                        BorderRadius.circular(15), // Rounded edges for aquarium
-                  ),
-                  child: Stack(
-                    children: fishList.map((fish) => fish.buildFish()).toList(),
-                  ),
-                ),
-              ),
-
-              SizedBox(height: screenHeight * 0.02), // Space after the aquarium
-
-              // Buttons and settings
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                    onPressed: _addFish,
-                    child: Text('Add Fish'),
-                  ),
-                  ElevatedButton(
-                    onPressed: _saveSettings,
-                    child: Text('Save Settings'),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            // Aquarium Container
+            Container(
+              width: screenWidth * 0.9,
+              height: screenHeight * 0.4,
+              decoration: BoxDecoration(
+                color: Colors.blue[300], // Aquarium background remains blue
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.3),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: Offset(0, 3),
                   ),
                 ],
               ),
-
-              SizedBox(height: screenHeight * 0.02), // Space after buttons
-
-              // Slider to control fish speed
-              Padding(
-                padding: EdgeInsets.symmetric(
-                    horizontal:
-                        screenWidth * 0.1), // Padding based on screen width
-                child: Slider(
-                  value: selectedSpeed,
-                  onChanged: (newSpeed) {
-                    setState(() {
-                      selectedSpeed = newSpeed;
-                    });
-                    _saveSettings(); // Save settings when speed changes
-                  },
-                  min: 0.5,
-                  max: 3.0,
-                  divisions: 5,
-                  label: '$selectedSpeed',
+              child: Stack(
+                children: fishList.map((fish) => fish.buildFish()).toList(),
+              ),
+            ),
+            SizedBox(height: 20),
+            
+            // Settings and Buttons Card
+            Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15)),
+              elevation: 4,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    // Buttons Row
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue,
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 24, vertical: 12),
+                          ),
+                          onPressed: _addFish,
+                          child: Text('Add Fish'),
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.green,
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 24, vertical: 12),
+                          ),
+                          onPressed: _saveSettings,
+                          child: Text('Save Settings'),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    // Slider for Fish Speed
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Fish Speed",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 16)),
+                        Slider(
+                          value: selectedSpeed,
+                          onChanged: (newSpeed) {
+                            setState(() {
+                              selectedSpeed = newSpeed;
+                            });
+                            _saveSettings();
+                          },
+                          min: 0.5,
+                          max: 3.0,
+                          divisions: 5,
+                          label: '$selectedSpeed',
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    // Dropdown for Fish Color
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Fish Color",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 16)),
+                        DropdownButton<Color>(
+                          value: selectedColor,
+                          items: colorOptions.keys.map((Color color) {
+                            return DropdownMenuItem<Color>(
+                              value: color,
+                              child: Text(
+                                colorOptions[color] ?? 'Unknown',
+                                style: TextStyle(color: color),
+                              ),
+                            );
+                          }).toList(),
+                          onChanged: (color) {
+                            setState(() {
+                              selectedColor = color ?? Colors.red;
+                            });
+                            _saveSettings();
+                          },
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    // Toggle switch for Collision Effect
+                    SwitchListTile(
+                      title: Text('Enable Collision Effect'),
+                      value: collisionEffectEnabled,
+                      onChanged: (bool value) {
+                        setState(() {
+                          collisionEffectEnabled = value;
+                        });
+                      },
+                    ),
+                  ],
                 ),
               ),
-
-              // Dropdown to select fish color with proper names
-              Padding(
-                padding: EdgeInsets.symmetric(
-                    horizontal:
-                        screenWidth * 0.1), // Padding based on screen width
-                child: DropdownButton<Color>(
-                  value:
-                      selectedColor, // Ensure selectedColor matches one of the items below
-                  items: colorOptions.keys.map((Color color) {
-                    return DropdownMenuItem<Color>(
-                      value: color,
-                      child: Text(
-                        colorOptions[color] ?? 'Unknown',
-                        style: TextStyle(color: color),
-                      ),
-                    );
-                  }).toList(),
-                  onChanged: (color) {
-                    setState(() {
-                      selectedColor = color ??
-                          Colors.red; // Default to red if color is null
-                    });
-                    _saveSettings(); // Save settings when color changes
-                  },
-                ),
-              ),
-
-              SizedBox(height: screenHeight * 0.02), // Space after dropdown
-
-              // Toggle switch for enabling/disabling collision effect
-              SwitchListTile(
-                title: Text('Enable Collision Effect'),
-                value: collisionEffectEnabled,
-                onChanged: (bool value) {
-                  setState(() {
-                    collisionEffectEnabled = value;
-                  });
-                },
-              ),
-            ],
-          ),
-
-          // Name and Panther ID centered at the bottom in a small rounded card
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-                color: Colors.white
-                    .withOpacity(0.8), // Slightly transparent background
-                elevation: 5,
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
+            ),
+            
+            SizedBox(height: 20),
+            // Footer Card with Name and Panther ID
+            Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15)),
+              color: Colors.grey[100],
+              elevation: 2,
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Center(
                   child: Text(
                     'CW05 Mahendra Krishna',
-                    textAlign: TextAlign.center,
                     style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.black,
-                    ),
+                        fontSize: 16, fontWeight: FontWeight.w500),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
